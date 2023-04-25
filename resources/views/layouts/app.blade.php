0d3fa085ed41bfd07c60d0e4df7d84888813c6b3
Mai Makabe <r.a.n.a9250@gmail.com>
2023-04-25 22:05:23 +0900

    UI/Profile pages fixed.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,32 +18,32 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
 
     <!--Fontawesome-->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
         integrity="sha512-gsfn6EdJ41hiX9Lzvbv31QaI1Yr7rGVxGc82o17Pn89F8Z/Y4m4nvC/ziv36f0mLW8Bbc7wFaaN0d0vruQZc5Q=="
         crossorigin="anonymous" />
-
+    {{-- icon of profile --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+    integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" /> --}}
+    <script src="https://cdn.tailwindcss.com/3.2.4"></script>
+    <script src="https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+
+    {{-- profile  --}}
+    {{-- <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" /> --}}
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
     <link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
 
 
 
-
-
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tw-elements/dist/css/index.min.css" />
-    <script src="https://cdn.tailwindcss.com/3.2.4"></script>
-    <script src="https://unpkg.com/@themesberg/flowbite@1.1.1/dist/flowbite.bundle.js"></script>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-
-
     <!-- style -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    {{-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> --}}
+
 </head>
 
 <body>
--- resources/views/profile/exchanges.blade.php
+++ resources/views/profile/exchanges.blade.php
@@ -3,98 +3,101 @@
 @include('after_login_nav')
 
 <main class="profile-page">
-  <section class="relative block h-500-px">
-    {{-- background-image --}}
-    <div class="absolute top-0 w-full h-full bg-center bg-cover" style="
+    <section class="relative block h-500-px">
+        {{-- background-image --}}
+        <div class="absolute top-0 w-full h-full bg-center bg-cover"
+            style="
             background-image: url('/assets/images/pexels-djordje-petrovic-2102415.jpg');
           ">
-      <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
-    </div>
-    <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style="transform: translateZ(0px)">
-      <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
-        <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
-      </svg>
-    </div>
-  </section>
-  <section class="relative py-16 bg-blueGray-200">
-
-    {{-- profile picture --}}
-    <div class="container mx-auto px-4">
-      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
-        <div class="px-6">
-          <div class="flex flex-wrap justify-center">
-            <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
-              <div class="relative">
-
-                <div class="bg-[url('/assets/images/pexels-djordje-petrovic-2102415.jpg')] shadow-xl rounded-full h-[150px] w-[150px] align-middle border-none absolute -m-16 -ml-20 bg-center bg-cover bg-no-repeat"></div>
-
-              </div>
-            </div>
-            {{-- button --}}
-            <div class="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-                <div class="py-6 px-3 mt-32 sm:mt-0">
-                    <button onclick="location.href='{{ url('#') }}'"
-                    class="bg-[#F6A5B3] hover:bg-[#ED4C59] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                    EDIT
-                    </button>
-                </div>
-              </div>
-          </div>
-          {{-- Name of teacher's  --}}
-          <div class="text-center mt-12 flex flex-col justify-center items-center">
-            <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
-              John Smith
-            </h3>
-            {{-- Address --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
-                Regidence/Country :
-              <span class="mb-2 text-blueGray-600">LA USA</span>
-            </div>
-            {{-- Age --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fa-solid fa-hourglass-half mr-2 text-lg text-blueGray-400"></i>
-              Age :
-              <span class="mb-2 text-blueGray-600">43</span>
-            </div>
-            {{-- Gender --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fa-solid fa-restroom mr-2 text-lg text-blueGray-400"></i>
-              Gender :
-              <span class="mb-2 text-blueGray-600">Male</span>
-            </div>
-            {{-- Occupation --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-              <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
-              Occupation :
-              <span class="mb-2 text-blueGray-600">Programmer</span>
-            </div>
-            {{-- Language --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fa-solid fa-microphone mr-2 text-lg text-blueGray-400"></i>
-                Language :
-                <span class="mb-2 text-blueGray-600">English</span>
-            </div>
-            {{-- Category --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fa-solid fa-microphone mr-2 text-lg text-blueGray-400"></i>
-                Category :
-                <span class="mb-2 text-blueGray-600">Programing</span>
-            </div>
-            {{--About me  --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
-                <i class="fa-solid fa-face-smile mr-2 text-lg text-blueGray-400"></i>
-                About me :
-                <span class="mb-2 text-blueGray-600">Hello.I would love to teach you about programing.</span>
-            </div>
-            {{-- Online/offline  --}}
-            <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold mb-6">
-                <i class="fa-solid fa-people-arrows mr-2 text-lg text-blueGray-400"></i>
-                Online/Offline :
-                <span class="mb-2 text-blueGray-600">Online</span>
-            </div>
+            <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
         </div>
-          {{-- <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+        <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
+            style="transform: translateZ(0px)">
+            <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"
+                version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+                <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+    </section>
+    <section class="relative py-16 bg-blueGray-200">
+
+        {{-- profile picture --}}
+        <div class="container mx-auto px-4">
+            <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+                <div class="px-6">
+                    <div class="flex flex-wrap justify-center">
+                        <div class="w-full flex justify-center">
+                            <div class="relative flex justify-center items-center w-full">
+                                <div
+                                    class="bg-[url('/assets/images/pexels-djordje-petrovic-2102415.jpg')] shadow-xl rounded-full h-[150px] w-[150px] align-middle border-none absolute -m-16 -ml-20 bg-center bg-cover bg-no-repeat">
+                                </div>
+
+                            </div>
+                        </div>
+                        {{-- button --}}
+                        <div class="flex items-end justify-end w-full py-10">
+                            <button onclick="location.href='{{ url('#') }}'"
+                                class="bg-[#F6A5B3] hover:bg-[#ED4C59] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                                EDIT
+                            </button>
+                        </div>
+                    </div>
+                    {{-- Name of teacher's  --}}
+                    <div class="text-center flex flex-col justify-center items-center">
+                        <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                            John Smith
+                        </h3>
+                        {{-- Address --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+                            Regidence/Country :
+                            <span class="mb-2 text-blueGray-600">LA USA</span>
+                        </div>
+                        {{-- Age --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fa-solid fa-hourglass-half mr-2 text-lg text-blueGray-400"></i>
+                            Age :
+                            <span class="mb-2 text-blueGray-600">43</span>
+                        </div>
+                        {{-- Gender --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fa-solid fa-restroom mr-2 text-lg text-blueGray-400"></i>
+                            Gender :
+                            <span class="mb-2 text-blueGray-600">Male</span>
+                        </div>
+                        {{-- Occupation --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>
+                            Occupation :
+                            <span class="mb-2 text-blueGray-600">Programmer</span>
+                        </div>
+                        {{-- Language --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fa-solid fa-microphone mr-2 text-lg text-blueGray-400"></i>
+                            Language :
+                            <span class="mb-2 text-blueGray-600">English</span>
+                        </div>
+                        {{-- Category --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fa-solid fa-microphone mr-2 text-lg text-blueGray-400"></i>
+                            Category :
+                            <span class="mb-2 text-blueGray-600">Programing</span>
+                        </div>
+                        {{-- About me  --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold">
+                            <i class="fa-solid fa-face-smile mr-2 text-lg text-blueGray-400"></i>
+                            About me :
+                            <span class="mb-2 text-blueGray-600">Hello.I would love to teach you about
+                                programing.</span>
+                        </div>
+                        {{-- Online/offline  --}}
+                        <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold mb-6">
+                            <i class="fa-solid fa-people-arrows mr-2 text-lg text-blueGray-400"></i>
+                            Online/Offline :
+                            <span class="mb-2 text-blueGray-600">Online</span>
+                        </div>
+                    </div>
+                    {{-- <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
             <div class="flex flex-wrap justify-center">
               <div class="w-full lg:w-9/12 px-4">
 
@@ -102,8 +105,8 @@
               </div>
             </div>
           </div> --}}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  </section>
+    </section>
 </main>
