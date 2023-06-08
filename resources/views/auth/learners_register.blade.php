56acad9a1ad5759a49b96ffb0cc4b258c49ef9e5
krs91tk <krs91tk@gmail.com>
2023-06-08 22:26:51 +0900

    fixed a form

--- resources/views/auth/learners_register.blade.php
+++ resources/views/auth/learners_register.blade.php
@@ -5,7 +5,7 @@
 @section('contents')
     <div class="h-screen w-screen bg-[url('/assets/images/register-modified.jpg')] bg-cover bg-center bg-no-repeat">
         <div class="max-w-md mx-auto flex items-center justify-center h-screen">
-            <form action="{{ route('learner.register') }}" method="post"
+            <form action="/{{ route('learner.register') }}" method="post"
                 class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-[500px]">
                 @csrf
                 <div class="text-center text-3xl font-bold mb-5">Learner's Register</div>
@@ -67,50 +67,50 @@
                 </div>
 
 
-        {{--  residence  --}}
-        <div class="mb-4">
-            <label class="block text-gray-700 font-bold mb-2" for="residence">
-                Residence
-            </label>
-            <input
-                class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="residence" type="text" placeholder="Enter your registration details" name="residence"
-                value="{{ old('residence') }}">
-            @error('residence')
-                <div class="text-red-500 text-xs">{{ $message }}</div>
-            @enderror
-        </div>
+                {{--  residence  --}}
+                <div class="mb-4">
+                    <label class="block text-gray-700 font-bold mb-2" for="residence">
+                        Residence
+                    </label>
+                    <input
+                        class="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                        id="residence" type="text" placeholder="Enter your registration details" name="residence"
+                        value="{{ old('residence') }}">
+                    @error('residence')
+                        <div class="text-red-500 text-xs">{{ $message }}</div>
+                    @enderror
+                </div>
 
-        {{--  gender radio button  --}}
-        <div class="mb-4 flex items-center">
-            <label class="block text-gray-700 font-bold mr-4" for="gender">
-                Gender
-            </label>
-            <div class="flex items-center">
-                <input type="radio" @if (old('gender') == 'male') checked @endif
-                    class="form-radio h-4 w-4 text-[#ED4C59] transition duration-150 ease-in-out" name="gender"
-                    value="male" checked>
-                <span class="ml-2 text-gray-700">Male</span>
-            </div>
-            <div class="flex items-center ml-4">
-                <input type="radio" @if (old('gender') == 'female') checked @endif
-                    class="form-radio h-4 w-4 text-[#ED4C59] transition duration-150 ease-in-out" name="gender"
-                    value="female">
-                <span class="ml-2 text-gray-700">Female</span>
-                @error('gender')
-                    <div class="text-red-500 text-xs">{{ $message }}</div>
-                @enderror
-            </div>
-        </div>
+                {{--  gender radio button  --}}
+                <div class="mb-4 flex items-center">
+                    <label class="block text-gray-700 font-bold mr-4" for="gender">
+                        Gender
+                    </label>
+                    <div class="flex items-center">
+                        <input type="radio" @if (old('gender') == 'male') checked @endif
+                            class="form-radio h-4 w-4 text-[#ED4C59] transition duration-150 ease-in-out" name="gender"
+                            value="male" checked>
+                        <span class="ml-2 text-gray-700">Male</span>
+                    </div>
+                    <div class="flex items-center ml-4">
+                        <input type="radio" @if (old('gender') == 'female') checked @endif
+                            class="form-radio h-4 w-4 text-[#ED4C59] transition duration-150 ease-in-out" name="gender"
+                            value="female">
+                        <span class="ml-2 text-gray-700">Female</span>
+                        @error('gender')
+                            <div class="text-red-500 text-xs">{{ $message }}</div>
+                        @enderror
+                    </div>
+                </div>
 
-        {{--  resister button  --}}
-        <div class="flex items-center justify-center">
-            <button type="submit"
-                class="bg-[#F6A5B3] hover:bg-[#ED4C59] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Register
-            </button>
+                {{--  resister button  --}}
+                <div class="flex items-center justify-center">
+                    <button type="submit"
+                        class="bg-[#F6A5B3] hover:bg-[#ED4C59] text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+                        Register
+                    </button>
+                </div>
+            </form>
         </div>
-        </form>
-    </div>
     </div>
 @endsection
